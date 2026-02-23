d41ee115fd2bc887dc53f91ecd81672948e79a54
RoboStar82 <star@robostar82.ru>
2026-02-23 09:41:31 +0300

    Оптимизация лидара

--- src/lidar.cpp
+++ src/lidar.cpp
@@ -1,57 +1,107 @@
 
 #include "lidar.h"
 
-/*
-void lidarSetup() {
-    lidar->setup();
-}
+Lidar::Lidar() {}
 
-void lidarBegin(void *params) {
-    while (true) {
-        lidar->loop();
-    }
-}
+Lidar::~Lidar() {}
 
-void lidarStart() {
-    lidar->control = lidarControlStart;
-    xTaskCreatePinnedToCore(lidarBegin, "lidar", 8192, NULL, 1, NULL, 0);
-}
-
-void lidarStop() {
-    lidar->control = lidarControlStop;
-}
-
-void lidarLoop() {
-    lidar->loop();
-}
-*/
-
-void RPLidar::begin() {
+void Lidar::begin() {
     pinMode(ROBOT_LIDAR_RX_PIN, INPUT);
     pinMode(ROBOT_LIDAR_TX_PIN, OUTPUT);
     LidarSerial.setTimeout(100);
     LidarSerial.begin(460800);
+    zero();
 }
 
-bool RPLidar::reset() {
+bool Lidar::start() {
     if (started) {
         return false;
     }
-    if (!sendCommand(0x40)) {
-        return false;
-    }
-    if (!AnyLidar::reset()) {
-        return false;
-    }
+    stopped = 0;
+    started = millis();
+    xTaskCreatePinnedToCore(task, "lidar_task", 8192, NULL, 1, NULL, 0);
     return true;
 }
 
-bool RPLidar::start() {
-    if (started) {
+bool Lidar::stop() {
+    if (stopped) {
         return false;
     }
+    started = 0;
+    stopped = millis();
+    return true;
+}
+
+void Lidar::task() {
+    int errors = 0;
+    while (!sendStartCommand()) {
+        vTaskDelay(1000);
+    }
+    zero();
+    while (!stopped) {
+        // 0..359
+        uint16_t angle = 0;
+        // 0..
+        uint16_t distance = 0;
+        // 0..
+        uint8_t strength = 0;
+        // Сканирование
+        if (scan(angle, distance, strength)) {
+            distances[angle] = distance;
+            errors = 0;
+        } else if (++errors > 999) {
+            log_e("Lidar: %d errors\n", errors);
+            sendStopCommand();
+            vTaskDelay(1000);
+            sendStartCommand();
+            errors = 0;
+        } else {
+            vTaskDelay(1);
+        }
+    }
+    zero();
+    while (!sendStopCommand()) {
+        vTaskDelay(1000);
+    }
+    vTaskDelete(NULL);
+}
+
+bool Lidar::scan(uint16_t& angle, uint16_t& distance, uint8_t& strength) {
+    if (!started) {
+        return false;
+    }
+    int count = 0;
+    while (LidarSerial.available() < 5) {
+        count++;
+        if (count > 1000) {
+            log_e("Lidar: read bytes (5)");
+            return false;
+        }
+        delay(1);
+    }
+    uint8_t data[5];
+    if (LidarSerial.readBytes(data, 5) < 5) {
+        log_e("Lidar: read bytes (5)");
+        return false;
+    }
+    if ((data[0] >> 0) & 0x01 == (data[0] >> 1) & 0x01) {
+        log_e("Lidar: scan 2 byte");
+        return false;
+    }
+    if (data[1] & 0x01 != 0x01) {
+        log_e("Lidar: check bit");
+        return false;
+    }
+    angle = ((data[1] | (data[2] << 8)) >> 7) % 360;
+    distance = (data[3] | (data[4] << 8)) >> 2;
+    strength = data[0] >> 2;
+    return true;
+}
+
+bool Lidar::sendStartCommand() {
     skipAll();
     if (!sendCommand(0x20)) {
+        skipAll();
         return false;
     }
     uint8_t type;
@@ -63,118 +113,30 @@ bool RPLidar::start() {
     if (length != 5) {
         return false;
     }
-    errorCount = 0;
-    started = millis();
     return true;
 }
 
-bool RPLidar::stop() {
-    started = 0;
+bool Lidar::sendResetCommand() {
+    skipAll();
+    if (!sendCommand(0x40)) {
+        skipAll();
+        return false;
+    }
+    skipAll();
+    return true;
+}
+
+bool Lidar::sendStopCommand() {
+    skipAll();
     if (!sendCommand(0x25)) {
         skipAll();
         return false;
-    } else {
-        skipAll();
-        return true;
     }
-}
-
-bool RPLidar::sendCommand(uint8_t command, uint8_t *payload, uint8_t size) {
-#if LIDAR_DEBUG_TX
-    if (payload != nullptr && size > 0) {
-        log_i("Lidar: tx: command=0x%02x, payload size=%d", command, size);
-    } else {
-        log_i("Lidar: tx: command=0x%02x", command);
-    }
-#endif
-    uint8_t checksum = 0;
-    if (LidarSerial.write(0xa5) < 1) {
-        log_e("Lidar: send byte (1) 0xa5");
-        return false;
-    }
-    checksum ^= 0xa5;
-    if (LidarSerial.write(command) < 1) {
-        log_e("Lidar: send byte (2) 0x%02x", command);
-        return false;
-    }
-    checksum ^= command;
-    if ((command & 0x80) || (payload != nullptr && size > 0)) {
-        if (LidarSerial.write(size) < 1) {
-            log_e("Lidar: send byte (3) 0x%02x", size);
-            return false;
-        }
-        checksum ^= size;
-        for (int i = 0; i < size; i++) {
-            if (LidarSerial.write(payload[i]) < 1) {
-                log_e("Lidar: send byte (%d) 0x%02x", i + 4, payload[i]);
-                return false;
-            }
-            checksum ^= payload[i];
-        }
-        if (LidarSerial.write(checksum) < 1) {
-            log_e("Lidar: send byte (%d) 0x%02x", size + 4, checksum);
-            return false;
-        }
-    }
+    skipAll();
     return true;
 }
 
-bool RPLidar::readHeader(uint8_t &type, uint8_t &subtype, uint32_t &length) {
-    uint8_t c;
-    if (LidarSerial.readBytes(&c, 1) < 1 || c != '\xa5') {
-        log_e("Lidar: read byte (1)");
-        return false;
-    }
-    if (LidarSerial.readBytes(&c, 1) < 1 || c != '\x5a') {
-        log_e("Lidar: read byte (2)");
-        return false;
-    }
-    if (LidarSerial.readBytes(&c, 1) < 1) {
-        log_e("Lidar: read byte (3)");
-        return false;
-    }
-    subtype = c >> 6;
-    length = c & 0x3f;
-    for (int i = 1; i < 4; i++) {
-        if (LidarSerial.readBytes(&c, 1) < 1) {
-            log_e("Lidar: read byte (%d)", i + 3);
-            return false;
-        }
-        subtype |= c << i;
-    }
-    if (LidarSerial.readBytes(&type, 1) < 1) {
-        log_e("Lidar: read byte (7)");
-        return false;
-    }
-#if LIDAR_DEBUG_RX
-    log_i("Lidar: rx: type=0x%02x, subtype=0x%02x, length=%d", type, subtype, length);
-#endif
-    return true;
-}
-
-bool RPLidar::skipAll() {
-    uint8_t c;
-    while (LidarSerial.available()) {
-        LidarSerial.readBytes(&c, 1);
-    }
-    return true;
-}
-
-bool RPLidar::skipBytes(size_t length) {
-    uint8_t c;
-    for (size_t i = 0; i < length; i++) {
-        if (LidarSerial.readBytes(&c, 1) < 1) {
-            log_e("Lidar: read byte (%d)", i);
-            return false;
-        }
-#if LIDAR_DEBUG_RX
-        log_i("Lidar: skip byte (%d) 0x%02x", i, c);
-#endif
-    }
-    return true;
-}
-
-bool RPLidar::getDeviceInfo() {
+bool Lidar::getDeviceInfo() {
     if (started) {
         return false;
     }
@@ -224,7 +186,7 @@ bool RPLidar::getDeviceInfo() {
     return true;
 }
 
-bool RPLidar::getDeviceHealth() {
+bool Lidar::getDeviceHealth() {
     if (started) {
         return false;
     }
@@ -262,11 +224,11 @@ bool RPLidar::getDeviceHealth() {
     return true;
 }
 
-bool RPLidar::getLidarConf(uint32_t conf) {
+bool Lidar::getLidarConf(uint32_t conf) {
     if (started) {
         return false;
     }
-    if (!sendCommand(0x84, (uint8_t *)(&conf), 4)) {
+    if (!sendCommand(0x84, (uint8_t*)(&conf), 4)) {
         return false;
     }
     uint8_t type;
@@ -282,36 +244,107 @@ bool RPLidar::getLidarConf(uint32_t conf) {
     return true;
 }
 
-bool RPLidar::scan(uint16_t &angle, uint16_t &distance, uint8_t &strength) {
-    if (!started) {
+bool Lidar::sendCommand(uint8_t command, uint8_t* payload, uint8_t size) {
+#if LIDAR_DEBUG_TX
+    if (payload != nullptr && size > 0) {
+        log_i("Lidar: tx: command=0x%02x, payload size=%d", command, size);
+    } else {
+        log_i("Lidar: tx: command=0x%02x", command);
+    }
+#endif
+    uint8_t checksum = 0;
+    if (LidarSerial.write(0xa5) < 1) {
+        log_e("Lidar: send byte (1) 0xa5");
         return false;
     }
-    int count = 0;
-    while (LidarSerial.available() < 5) {
-        count++;
-        if (count > 1000) {
-            log_e("Lidar: read bytes (5)");
+    checksum ^= 0xa5;
+    if (LidarSerial.write(command) < 1) {
+        log_e("Lidar: send byte (2) 0x%02x", command);
+        return false;
+    }
+    checksum ^= command;
+    if ((command & 0x80) || (payload != nullptr && size > 0)) {
+        if (LidarSerial.write(size) < 1) {
+            log_e("Lidar: send byte (3) 0x%02x", size);
             return false;
         }
-        delay(1);
+        checksum ^= size;
+        for (int i = 0; i < size; i++) {
+            if (LidarSerial.write(payload[i]) < 1) {
+                log_e("Lidar: send byte (%d) 0x%02x", i + 4, payload[i]);
+                return false;
+            }
+            checksum ^= payload[i];
+        }
+        if (LidarSerial.write(checksum) < 1) {
+            log_e("Lidar: send byte (%d) 0x%02x", size + 4, checksum);
+            return false;
+        }
     }
-    uint8_t data[5];
-    if (LidarSerial.readBytes(data, 5) < 5) {
-        log_e("Lidar: read bytes (5)");
-        return false;
-    }
-    if ((data[0] >> 0) & 0x01 == (data[0] >> 1) & 0x01) {
-        log_e("Lidar: scan 2 byte");
-        return false;
-    }
-    if (data[1] & 0x01 != 0x01) {
-        log_e("Lidar: check bit");
-        return false;
-    }
-    angle = ((data[1] | (data[2] << 8)) >> 7) % 360;
-    distance = (data[3] | (data[4] << 8)) >> 2;
-    strength = data[0] >> 2;
     return true;
+}
+
+bool Lidar::readHeader(uint8_t& type, uint8_t& subtype, uint32_t& length) {
+    uint8_t c;
+    if (LidarSerial.readBytes(&c, 1) < 1 || c != '\xa5') {
+        log_e("Lidar: read byte (1)");
+        return false;
+    }
+    if (LidarSerial.readBytes(&c, 1) < 1 || c != '\x5a') {
+        log_e("Lidar: read byte (2)");
+        return false;
+    }
+    if (LidarSerial.readBytes(&c, 1) < 1) {
+        log_e("Lidar: read byte (3)");
+        return false;
+    }
+    subtype = c >> 6;
+    length = c & 0x3f;
+    for (int i = 1; i < 4; i++) {
+        if (LidarSerial.readBytes(&c, 1) < 1) {
+            log_e("Lidar: read byte (%d)", i + 3);
+            return false;
+        }
+        subtype |= c << i;
+    }
+    if (LidarSerial.readBytes(&type, 1) < 1) {
+        log_e("Lidar: read byte (7)");
+        return false;
+    }
+#if LIDAR_DEBUG_RX
+    log_i("Lidar: rx: type=0x%02x, subtype=0x%02x, length=%d", type, subtype, length);
+#endif
+    return true;
+}
+
+bool Lidar::skipBytes(size_t length) {
+    uint8_t c;
+    for (size_t i = 0; i < length; i++) {
+        if (LidarSerial.readBytes(&c, 1) < 1) {
+            log_e("Lidar: read byte (%d)", i);
+            return false;
+        }
+#if LIDAR_DEBUG_RX
+        log_i("Lidar: skip byte (%d) 0x%02x", i, c);
+#endif
+    }
+    return true;
+}
+
+bool Lidar::skipAll() {
+    uint8_t c;
+    while (LidarSerial.available()) {
+        LidarSerial.readBytes(&c, 1);
+    }
+    return true;
+}
+
+void Lidar::zero() {
+    memset(distances, 0, sizeof(distances));
+}
+
+void Lidar::task(void* arg) {
+    lidar.task();
 }
 
 Lidar lidar = Lidar();

fe762831d94f56946b94cb5835250b258bcde26e
IvannPI <ivannpeshcoff@yandex.ru>
2026-02-25 20:02:24 +0300

    Совместимость со старым Питоном

--- version.py
+++ version.py
@@ -6,5 +6,5 @@ with open(os.path.join('include', 'version.h'), 'w') as F:
     now = datetime.datetime.now()
     F.write('\n')
     F.write('#define BUILD_DATETIME "{:%Y-%m-%d %H:%M:%S}"\n'.format(now))
-    F.write('#define BUILD_TIMESTAMP {:%s}\n'.format(now))
+    F.write('#define BUILD_TIMESTAMP ' + str(int(now.timestamp())) + '\n')
     F.close()

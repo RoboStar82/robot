16c7e2c0287a63135450dddda890b8dcda64ead6
RoboStar82 <star@robostar82.ru>
2026-08-03 14:59:19 +0300

    Скорректированы настройки

--- include/config.h
+++ include/config.h
@@ -2,21 +2,21 @@
 #pragma once
 
 #ifdef ARDUINO_DIYMORE_ESP32_S3_N16R8
-#include "config_diymore_esp32s3n16r8.h"
+#include "config/diymore_esp32s3n16r8.h"
 #endif
 
 #ifdef ARDUINO_EMAKEFUN_ESP32_WROOM_32E
-#include "config_emakefun_esp32_wroom_32e.h"
+#include "config/emakefun_esp32_wroom_32e.h"
 #endif
 
 #ifdef ARDUINO_HELTEC_VISION_MASTER_E213
-#include "config_heltec_vision_master_e213.h"
+#include "config/heltec_vision_master_e213.h"
 #endif
 
 #ifdef ARDUINO_HELTEC_WIRELESS_STICK_LITE_V3
-#include "config_heltec_wireless_stick_lite_v3.h"
+#include "config/heltec_wireless_stick_lite_v3.h"
 #endif
 
 #ifdef ARDUINO_WEACT_MINI_H743VIT6
-#include "config_weact_mini_stm32h743vit6.h"
+#include "config/weact_mini_stm32h743vit6.h"
 #endif
